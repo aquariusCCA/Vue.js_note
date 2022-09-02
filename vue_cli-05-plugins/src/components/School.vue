1a0d9d5690a58b02f53e08e0882ae1b261b43e75
DESKTOP-QTBPETG\linki <lin860210@i-mps.com>
2022-09-02 21:44:37 +0800

    提交 Vue 路由

---
+++ vue_cli-05-plugins/src/components/School.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2 @click="showName">學校名稱: {{name}}</h2>    
+        <h2>學生地址: {{address}}</h2>    
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Student',
+        data() {
+            return {
+                name: '尚硅谷',
+                address: '北京-昌平',
+            }
+        }
+    }
+</script>
---
+++ vue_cli-05-plugins/src/components/Student.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2 @click="showName">學生姓名: {{name}}</h2>    
+        <h2>學生性別: {{sex}}</h2>    
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Student',
+        data() {
+            return {
+                name: '蕭士麟',
+                sex: '男'
+            }
+        }
+    }
+</script>
