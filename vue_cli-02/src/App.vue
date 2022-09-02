b807eb29b107927ebe4df571326c40a74b670138
DESKTOP-QTBPETG\linki <lin860210@i-mps.com>
2022-09-02 17:20:12 +0800

    提交 refs屬性

---
+++ vue_cli-02/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1 v-text="msg" ref="title"></h1>
+        <button ref="btn" @click="showDOM">點我輸出上方的 DOM 元素</button>
+        <School ref="sch"></School>
+    </div>
+</template>
+
+<script>
+    import School from './components/School.vue'
+
+    export default {
+        name: 'App',
+        data(){
+            return {
+                msg: '歡迎學習 Vue'
+            }
+        },
+        components: { // 註冊組件
+            School
+        },
+        methods: {
+            // ref 屬性:
+            //     1. 被用來給元素或子組件註冊引用信息(id的替代者)
+            //     2. 應用在 html 標籤上獲取的是真實 DOM 元素，應用在組件標籤上是組件實例對象
+            //     3. 使用方式
+            //         打標識: <h1 ref="xxx">....</h1>
+            //         獲取: this.$refs.xxx
+            showDOM(){
+                console.log(this.$refs)
+                console.log(this.$refs.title) // 真實 DOM 元素。 
+                console.log(this.$refs.btn) // 真實 DOM 元素。
+                console.log(this.$refs.sch) // School 組件對象。
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ vue_cli-02/src/components/School.vue
@@ -0,0 +1,27 @@
+<template>
+    <!-- div 標籤中的 class="school" -> 指定樣式名稱 -->
+    <div class="school">
+        <h2>學校名稱: {{name}}</h2>
+        <h2>學校地址: {{address}}</h2>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        // name: 'School'  ->  組件名稱
+        name: 'School',
+        data(){
+            return {
+                name: '尚硅谷',
+                address: '北京-昌平'
+            }
+        }
+    }
+</script>
+
+<style>
+    .school {
+        background-color: orange;
+    }
+</style> 
